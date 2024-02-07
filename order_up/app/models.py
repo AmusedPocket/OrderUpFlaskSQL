6079545b206b33f39b7e92600db1b67f94d451a1
NickyViNi <happylemonlcm@gmail.com>
2024-02-07 15:39:48 -0800

    password

--- order_up/app/models.py
+++ order_up/app/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin  
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+# import hashlib
 
 db = SQLAlchemy()
 
@@ -18,7 +19,6 @@ class Employee(db.Model, UserMixin):
     @password.setter
     def password(self, password):
         self.hashed_password = generate_password_hash(password)
-    
+
     def check_password(self, password):
         return check_password_hash(self.password, password)
-    
